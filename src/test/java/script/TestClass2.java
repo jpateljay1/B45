1704c835d139933cb75379aecf0e208b7a73e306
PERSONAL <PERSONAL@192.168.0.203>
2022-07-26 20:50:00 +0530

    Draft copy

---
+++ src/test/java/script/TestClass2.java
@@ -0,0 +1,19 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import generic.BaseTest;
+
+public class TestClass2 extends BaseTest
+{
+
+	@Test
+	public void testLogin() {
+		
+		extentTest.log(Status.INFO, "This is TestB");
+	}
+}
